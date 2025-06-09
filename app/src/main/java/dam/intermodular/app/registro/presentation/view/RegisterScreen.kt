35ba73c2373d70d913a8d927c5aa1048182811bf
JLD2024 <158503356+JLD2024@users.noreply.github.com>
2025-06-09 12:42:19 +0200

    Update style RegisterScreen.kt

--- app/src/main/java/dam/intermodular/app/registro/presentation/view/RegisterScreen.kt
+++ app/src/main/java/dam/intermodular/app/registro/presentation/view/RegisterScreen.kt
@@ -1,5 +1,7 @@
 package dam.intermodular.app.registro.presentation.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +12,9 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import dam.intermodular.app.login.presentation.composables.EmailTextFiel
@@ -23,42 +28,58 @@ fun RegisterScreen(navigateTo: NavController,viewModel: RegisterViewModel){
     val email: String by viewModel.email.collectAsState()
     val password:  String by viewModel.password.collectAsState()
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment =  Alignment.CenterHorizontally
-    ){
-        Spacer(modifier = Modifier.weight(1f))
-        Text(
-            text = "Register",
-            fontSize = 25.sp
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = dam.intermodular.app.R.drawable.fondo), // Replace with your image
+            contentDescription = "Background",
+            modifier = Modifier.fillMaxSize()
         )
-        Spacer(modifier = Modifier.weight(1f))
-        Text(
-            text = "Email",
-            fontSize = 20.sp
-        )
-        EmailTextFiel(email) { newEmail ->
-            viewModel.onRegisterChange(newEmail, password)
-        }
-        Spacer(modifier = Modifier.weight(1f))
-        Text(
-            text = "Password",
-            fontSize = 20.sp
-        )
-        PasswordTextField (password) { newPassword ->
-            viewModel.onRegisterChange(email,newPassword)
-        }
-        Spacer(modifier = Modifier.weight(1f))
-        Button(
-            onClick = {  viewModel.registerUser(navigateTo) }
-        ) {
+
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment =  Alignment.CenterHorizontally
+        ){
+            Spacer(modifier = Modifier.weight(1f))
             Text(
-                text = "Register",
-                fontSize = 20.sp
+                text = "Registrar",
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
             )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "Email",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            EmailTextFiel(email) { newEmail ->
+                viewModel.onRegisterChange(newEmail, password)
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "Password",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            PasswordTextField (password) { newPassword ->
+                viewModel.onRegisterChange(email,newPassword)
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = {  viewModel.registerUser(navigateTo) }
+            ) {
+                Text(
+                    text = "Register",
+                    fontSize = 20.sp
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+
+
         }
-        Spacer(modifier = Modifier.weight(1f))
-
-
     }
 }
